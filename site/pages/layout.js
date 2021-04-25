086226adc146d7fe0789f0d08d3a80200f70983c
MikielAgutu <mikielagutu@yahoo.co.uk>
2021-04-25 14:14:23 +0100

    changes

--- site/pages/layout.js
+++ site/pages/layout.js
@@ -10,6 +10,20 @@ export default function Layout({children}) {
       <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.1/dist/mini-default.min.css" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap" rel="stylesheet" />
+
+      {/* Google Analytics */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LY4TN960M8"></script>
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-LY4TN960M8');
+        `
+          }}
+        />
     </Head>
 
     <nav role="navigation">
